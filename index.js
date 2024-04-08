d5f270a65a336d56d9e39e985df1955e6df888b9
upulkr <upulkr4@gmail.com>
2024-04-08 11:34:05 +0530

    name eror

--- index.js
+++ index.js
@@ -18,12 +18,12 @@ app.use(express.urlencoded({ extended: true }));
 
 app.post("/api", async (req, res) => {
   try {
-    const { fullname, email, phoneNumber, message } = req.body;
+    const { name, email, phoneNumber, message } = req.body;
 
     // Assuming EmailSender returns a promise
-    await EmailSender({fullname,email,phoneNumber,message });
+    await EmailSender({name,email,phoneNumber,message });
 
-    res.json({ msg: `Your message sent successfully. ${fullname}` });
+    res.json({ msg: `Your message sent successfully. ${name}` });
   } catch (error) {
     console.error(error);
     res.status(500).json({ msg: "Internal Server Error ❌" });
--- sendMail.js
+++ sendMail.js
@@ -20,7 +20,7 @@ const Email = (options) => {
   });
 };
 
-const EmailSender = ({ fullname, email, phoneNumber, message }) => {
+const EmailSender = ({ name, email, phoneNumber, message }) => {
   const customerDetailsOptions = {
     from: `Customer Request to Altra Scientific`,
     to: process.env.SEND_TO,
@@ -31,10 +31,10 @@ const EmailSender = ({ fullname, email, phoneNumber, message }) => {
          
             <div style="width: 100%; gap: 10px; padding: 30px 0; display: grid">
               <p style="font-weight: 800; font-size: 1.2rem; padding: 0 30px">
-              From ${fullname}
+              From ${name}
               </p>
               <div style="font-size: .8rem; margin: 0 30px">
-                <p>name: <b>${fullname}</b></p>
+                <p>name: <b>${name}</b></p>
                 <p>Email: <b>${email}</b></p>
                 <p>phoneNumber: <b>${phoneNumber}</b></p>
                 <p>Message: <i>${message}</i></p>
@@ -51,7 +51,7 @@ const EmailSender = ({ fullname, email, phoneNumber, message }) => {
     html: `
       <div style=" padding: 15px; border-top: 4px solid #D97706; border-radius: 8px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);">
             <p style="font-size: 18px; font-weight: bold; color: #1F2937;">
-                Hi ${fullname}, thank you for reaching out to Altra Scientific!
+                Hi ${name}, thank you for reaching out to Altra Scientific!
             </p>
             <p style="margin-top: 10px; font-size: 16px; color: #6B7280;">
                 We've received your message, and our dedicated customer service team is diligently working to provide you with a prompt and satisfactory response. Your satisfaction is our priority, and we appreciate your patience.
